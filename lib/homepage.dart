3be4ae2209291f3456a93ced5c9b17b66c5b6239
Khizar-PC <khizerdawood6@gmail.com>
2020-12-05 16:17:57 +0500

    Simple Level App

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,8 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:sp17bcs040/simple.dart';
+import 'package:sp17bcs040/hard.dart';
 
-class homepage extends StatelessWidget {
+class homepage extends StatefulWidget {
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black12,
+      appBar: AppBar(
+        title: Text(
+          "RoLLing Dice",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Color(0xFFCA6924),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "GAME LEVELS",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 38.0,
+                ),
+              ),
+              Container(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(50),
+                      child: RaisedButton(
+                        padding: EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
+                        child: Text(
+                          'SIMPLE',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        color: Color(0xFFE2B13C),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => simple()),
+                          );
+                        },
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: RaisedButton(
+                        padding: EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 20.0),
+                        child: Text(
+                          'HARD',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        color: Color(0xFFE2B13C),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => hard()),
+                          );
+                        },
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
